f41694c874f3a6318eb6f06c8ddf3e4c1131805c
Brian Sinquin <brian.sinquin@gmail.com>
2022-08-04 17:17:31 +0200

    Literate

---
+++ jmd/test.jl
@@ -0,0 +1,34 @@
+# ---
+# title: "Le notbook Julia"
+# date: 2022-06-05T15:26:35+02:00
+# lastmod: 2022-06-05T15:26:35+02:00
+# draft: false
+# author: "Brian SINQUIN"
+# authorLink: "https://www.linkedin.com/in/brian-sinquin/"
+# description: "Un test"
+#
+# tags: []
+# categories: ["physique", "informatique","programmation"]
+#
+# hiddenFromHomePage: false
+# hiddenFromSearch: false
+#
+#
+#
+# toc:
+#   enable: true
+#
+# lightgallery: false
+# license: "MIT"
+# ---
+#
+# <!--more-->
+#
+# ## Comming soon ⌚
+
+using Plots
+
+x = 1:0.1:10
+y = sin.(x)
+
+plot(x,y)
---
+++ make.jl
@@ -0,0 +1,23 @@
+using Pkg;
+
+Pkg.add("Literate")
+Pkg.add("Plots")
+
+using Literate, Plots
+
+PREFIX = "J_"
+JMD = joinpath("../",@__DIR__, "jmd")
+MD_OUTPUT = joinpath(@__DIR__, "content\\posts")
+
+for file in readdir(MD_OUTPUT, join=false)
+    println(file)
+    if startswith(file, PREFIX)
+        rm(joinpath(MD_OUTPUT,file), recursive=true)
+    end
+end
+
+
+for file in readdir(JMD; join=false)
+    endswith(file, ".jl") || continue
+    Literate.markdown(joinpath(JMD,file), joinpath(MD_OUTPUT,PREFIX*file[1:end-2]), name="index"; execute=true,flavor = Literate.CommonMarkFlavor())
+end
