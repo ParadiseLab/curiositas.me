9440e1446eb7b4052b0bc65fb02a781dd206a49d
Brian Sinquin <brian.sinquin@gmail.com>
2022-08-05 12:38:30 +0200

    test

--- jmd/add_literate_hugo.jl
+++ jmd/add_literate_hugo.jl
@@ -1,14 +1,14 @@
 # ---
-# title: "Le notbook Julia"
-# date: 2022-06-05T15:26:35+02:00
-# lastmod: 2022-06-05T15:26:35+02:00
+# title: "Intégrer Julia dans Hugo avec Literate.jl"
+# date: 2022-08-05T15:26:35+02:00
+# lastmod: 2022-08-05T15:26:35+02:00
 # draft: false
 # author: "Brian SINQUIN"
 # authorLink: "https://www.linkedin.com/in/brian-sinquin/"
 # description: "Un test"
 #
 # tags: []
-# categories: ["physique", "informatique","programmation"]
+# categories: ["informatique","programmation"]
 #
 # hiddenFromHomePage: false
 # hiddenFromSearch: false
@@ -24,14 +24,12 @@
 #
 # <!--more-->
 #
-# ## Comming soon ⌚
+# ## En cours d'écriture ✍️
 
+# Ceci est un exemple de code julia et son résultat une fois exécuté
 using Plots
 
 x = 1:0.1:10
 y = sin.(x)
 
-plot(x,y);
-savefig("plot.png");
-
-# ![Figure](plot.png)
+plot(x,y)
